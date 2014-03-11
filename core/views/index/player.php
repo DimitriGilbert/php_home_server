15b7aabfeadd9b89584b6f36c978e43e657bc9f7
Dimitri Gilbert <dimitri.gilbert@gmail.com>
2014-03-12 00:55:02 +0800

    fix on tag name in player view

--- core/views/index/player.php
+++ core/views/index/player.php
@@ -1,7 +1,9 @@
 <<?php
+// creating video or audio tag acoording to media type
 switch ($type) 
 {
 	case 'video':
+		//i should put that in a config
 		echo 'video width="240" height="180"';
 		break;
 	case 'music':
@@ -11,4 +13,14 @@ switch ($type)
 ?> controls>
   <source src="<?php echo $file; ?>">
 Your browser does not support the video/audio tag.
-</video> 
+</<?php
+switch ($type) 
+{
+	case 'video':
+		echo 'video';
+		break;
+	case 'music':
+		echo 'audio';
+		break;
+}
+?>> 
